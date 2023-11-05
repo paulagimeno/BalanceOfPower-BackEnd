b6cdfb35b8911caab4470ebc1ada66ca77c11348
paulagimeno <paula-gl-98@hotmail.com>
2023-11-05 12:10:19 +0100

    updateAvatar

--- src/api/routes/CharacterRoute.js
+++ src/api/routes/CharacterRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getCharacters, addCharacter, updateCharacter, deleteCharacter } = require('../controllers/CharacterControllers');
+const { getCharacters, addCharacter, updateCharacter, deleteCharacter, updateAvatar } = require('../controllers/CharacterControllers');
 const upload = require('../../middleware/upload.files');
 const router = express.Router();
 
@@ -7,6 +7,8 @@ const router = express.Router();
 router.post('/addCharacter', upload.single('avatarImage'), addCharacter);
 router.get('/characters', getCharacters);
 router.put('/updateCharacter/:id', upload.single('fullBodyImage'), updateCharacter)
+router.put('/updateAvatar/:id', upload.single('avatarImage'), updateAvatar)
+
 router.delete('/:id', deleteCharacter);
 
 module.exports = router
